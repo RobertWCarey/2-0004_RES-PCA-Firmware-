ace0173aa65345a618133b656298ee917504413c
Rob Carey <rob_carey_1@hotmail.com>
2020-05-06 19:32:29 +1000

    Added display init
    - working with example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include "PWM.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 typedef union
 {
@@ -26,6 +30,35 @@ int32_t Freq = defaultFreq;
 String Command;
 
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
 
 // Gets value to set analogWrite function
 int getAWrite(int32_t freq,int duty);
@@ -74,6 +107,7 @@ void UI_updateDisplay(uint16union_t displayState)
 
 void UI_mainMenu(void)
 {
+  
 
 }
 
@@ -164,7 +198,27 @@ void PWMInit(void)
 
 void UI_init(void)
 {
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
 
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
 }
 
 void setup()
@@ -172,6 +226,8 @@ void setup()
   Serial.begin(9600);
 
   PWMInit();
+
+  UI_init();
 }
 
 void loop()
