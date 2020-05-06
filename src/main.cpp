caed0e7008c05dba387fa40aff708f41b8204338
Rob Carey <rob_carey_1@hotmail.com>
2020-05-06 18:24:48 +1000

    Created init funcs and start of UI logic
    -PWMInit() just moved from setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <Arduino.h>
 #include "PWM.h"
 
+typedef union
+{
+  uint16_t l;
+  struct
+  {
+    uint8_t Lo;
+    uint8_t Hi;
+  } s;
+} uint16union_t;
+
 // Processor Frequency
 int32_t clkFreq = 16000000;
 
@@ -15,6 +25,8 @@ int Duty = defaultDuty;
 int32_t Freq = defaultFreq;
 String Command;
 
+
+
 // Gets value to set analogWrite function
 int getAWrite(int32_t freq,int duty);
 // Updates the duty cycle for pin_PWM
@@ -24,22 +36,45 @@ void setFreq(int32_t freq);
 // Parses any serial commands recieved
 bool parseCommand(String com);
 
-void setup()
+void PWMInit(void);
+
+void UI_init(void);
+
+uint16union_t DisplayState;
+void UI_updateDisplay(uint16union_t displayState);
+
+void UI_mainMenu(void);
+
+void UI_updateDisplay(uint16union_t displayState)
 {
-  Serial.begin(9600);
-  //initialize all timers except for 0, to save time keeping functions
-  InitTimersSafe();
-  //sets the frequency for the specified pin
-  bool success = SetPinFrequencySafe(pin_PWM, defaultFreq);
-   if(success) {
-      pinMode(13, OUTPUT);
-      digitalWrite(13, HIGH);
-      pinMode(pin_PWM,OUTPUT);
-      pinMode(pin_PWM2,OUTPUT);
-   }
-   TCCR1A |= _BV(COM1A0);
-  analogWrite(pin_PWM,getAWrite(defaultFreq,defaultDuty));
-  analogWrite(pin_PWM2,getAWrite(defaultFreq,defaultDuty));
+  uint8_t mainState = displayState.s.Hi;
+  uint8_t subState = displayState.s.Lo;
+  switch (mainState)
+  {
+    // Frequency
+    case 1:
+      switch (subState)
+      {
+        case 0:
+          
+          break;
+        // Adjust Frequency
+        case 1:
+          break;
+        default:
+          break;
+      }
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
+}
+
+void UI_mainMenu(void)
+{
+
 }
 
 int getAWrite(int32_t freq,int duty)
@@ -108,6 +143,35 @@ bool parseCommand(String com)
   }
 
   return false;
+}
+
+void PWMInit(void)
+{
+  //initialize all timers except for 0, to save time keeping functions
+  InitTimersSafe();
+  //sets the frequency for the specified pin
+  bool success = SetPinFrequencySafe(pin_PWM, defaultFreq);
+   if(success) {
+      pinMode(13, OUTPUT);
+      digitalWrite(13, HIGH);
+      pinMode(pin_PWM,OUTPUT);
+      pinMode(pin_PWM2,OUTPUT);
+   }
+   TCCR1A |= _BV(COM1A0);
+  analogWrite(pin_PWM,getAWrite(defaultFreq,defaultDuty));
+  analogWrite(pin_PWM2,getAWrite(defaultFreq,defaultDuty));
+}
+
+void UI_init(void)
+{
+
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  PWMInit();
 }
 
 void loop()
